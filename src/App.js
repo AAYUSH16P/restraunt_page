0cb30d1ffe63307a281bb471808d3158ec819114
Aayush <you@example.com>
2022-12-02 02:13:47 +0530

    restraunt page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GalleryReact from "./component/TabMenu/GalleryReact";
+
+const App = () => {
+  return (
+    <>
+     <GalleryReact />
+    </>
+  )
+}
+
+export default App;
